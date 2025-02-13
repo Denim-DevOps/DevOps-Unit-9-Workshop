55c329306ed992ebce34502836052f6ac566de7f
Denim <denim.moran@gmail.com>
2025-02-13 16:05:28 +0000

    commit #2 - starting 4.2 in instructions

--- main.tf
+++ main.tf
@@ -43,3 +43,27 @@ resource "azurerm_linux_web_app" "main" {
     "CONNECTION_STRING" : "Server=tcp:denim-non-iac-sqlserver.database.windows.net,1433;Initial Catalog=denim-non-iac-db;Persist Security Info=False;User ID=dbadmin;Password=${var.database_password};...;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
   }
 }
+
+resource "azurerm_mssql_server" "main" {
+  name                         = "denim-non-iac-sqlserver"
+  resource_group_name          = data.azurerm_resource_group.main.name
+  location                     = data.azurerm_resource_group.main.location
+  version                      = "12.0"
+  administrator_login          = "dbadmin"
+  administrator_login_password = var.database_password
+}
+
+resource "azurerm_mssql_database" "main" {
+  name         = "denim-non-iac-db"
+  server_id    = azurerm_mssql_server.main.id
+  collation    = "SQL_Latin1_General_CP1_CI_AS"
+  license_type = "LicenseIncluded"
+  max_size_gb  = 2
+  sku_name     = "S0"
+  enclave_type = "VBS"
+
+  tags = {
+    foo = "bar"
+}
+
+}
